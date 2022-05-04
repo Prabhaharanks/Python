2e1c4e18b06b27dcc630282907b2f0322a45e229
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-05-04 17:26:52 +0530

    Create IfelifElse.py

---
+++ ControlStatementsAndLoops/IfelifElse.py
@@ -0,0 +1,9 @@
+n= 1122
+if 9 < n < 99:
+    print("Two digit number")
+elif 99 < n < 999:
+    print("Three digit number")
+elif 999 < n < 9999:
+    print("Four digit number")
+else:
+    print("number is <= 9 or >= 9999")
