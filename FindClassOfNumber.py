47f66cbc742ea938416bed23add0866fcb06a87a
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-06-01 22:17:51 +0530

    Create FindClassOfNumber.py

---
+++ FindClassOfNumber.py
@@ -0,0 +1,12 @@
+# program to find the class of a number
+# int
+num = 100
+print("type of num: ",type(num))
+
+# float
+num2 = 10.99
+print("type of num2: ",type(num2))
+
+# complex numbers
+num3 = 3 + 4j
+print("type of num3: ",type(num3))
