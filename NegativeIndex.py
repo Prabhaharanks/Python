319edec4585a6f3e2fb076da8fc138cab36a08a8
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-06-16 22:00:29 +0530

    Create NegativeIndex.py

---
+++ NegativeIndex.py
@@ -0,0 +1,12 @@
+# Negative index of list 
+# a list of strings
+my_list = ["hello", "world", "hi", "bye"]
+
+# prints "bye"
+print(my_list[-1])
+
+# prints "world"
+print(my_list[-3])
+
+# prints "hello"
+print(my_list[-4])
