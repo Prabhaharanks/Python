77dc36f653d6db797003dff29a24139dd1679f71
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-06-18 22:45:32 +0530

    Create Slicing.py

---
+++ Slicing.py
@@ -0,0 +1,15 @@
+# Slicing Example 
+# list of numbers
+n_list = [1, 2, 3, 4, 5, 6, 7]
+
+# list items from 2nd to 3rd
+print(n_list[1:3])
+
+# list items from beginning to 3rd
+print(n_list[:3])
+
+# list items from 4th to end of list
+print(n_list[3:])
+
+# Whole list
+print(n_list[:])
