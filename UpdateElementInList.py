3b94d411cef675b198a57d37a2962e64fa30add6
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-06-20 22:35:05 +0530

    Create UpdateElementInList.py

---
+++ UpdateElementInList.py
@@ -0,0 +1,15 @@
+# Update element in the list 
+# list of numbers
+n_list = [1, 2, 3, 4]
+
+# Changing the value of 3rd item
+n_list[2] = 100
+
+# list: [1, 2, 100, 4]
+print(n_list)
+
+# Changing the values of 2nd to fourth items
+n_list[1:4] = [11, 22, 33]
+
+# list: [1, 11, 22, 33]
+print(n_list)
