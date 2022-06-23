215e112f101140c1fd96e358172f137f5da899d2
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-06-23 23:51:42 +0530

    Create DeletingList.py

---
+++ DeletingList.py
@@ -0,0 +1,18 @@
+# Deleting list 
+# list of numbers
+n_list = [1, 2, 3, 4, 5, 6]
+
+# Deleting 2nd element
+del n_list[1]
+
+# list: [1, 3, 4, 5, 6]
+print(n_list)
+
+# Deleting elements from 3rd to 4th
+del n_list[2:4]
+
+# list: [1, 3, 6]
+print(n_list)
+
+# Deleting the whole list
+del n_list
