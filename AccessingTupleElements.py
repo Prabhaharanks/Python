91a560ee36fe7acc73e52325a0f9f5832469c3c4
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2022-06-29 22:19:06 +0530

    Create AccessingTupleElements.py

---
+++ AccessingTupleElements.py
@@ -0,0 +1,14 @@
+#Accessing the tuple elements 
+# tuple of strings
+my_data = ("hi", "hello", "bye")
+
+# displaying all elements
+print(my_data)
+
+# accessing first element
+# prints "hi"
+print(my_data[0])
+
+# accessing third element
+# prints "bye"
+print(my_data[1])
